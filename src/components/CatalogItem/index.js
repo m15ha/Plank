faf31d5dec7fd5de4c89f9a423058d8c6f3e7edd
m15ha <mihail.lazareff@gmail.com>
2022-02-12 16:25:38 -0500

    chore: style category list

--- src/components/CatalogItem/index.js
+++ src/components/CatalogItem/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import defaultImg from '../../assets/img/default.png';
+import arrowIcon from '../../assets/arrow-icon.svg';
 
 const CatalogItem = ({ caseStudy }) => {
     const { id, title, link, thumbnail, excerpt, categories } = caseStudy;
+
+    const img = thumbnail ? thumbnail : defaultImg;
+
     return (
-        <li className='item__container'>
-            <div className='item__ibg'>
-                <img className='item__image' src={thumbnail} alt={title} />
+        <div className={styles.item__container}>
+            <div className={styles.item__image}>
+                <img className={styles.item__ibg} src={img} alt={title} />
             </div>
-            <div className='item__content'>
-                <div className='item__content-category'>{categories.title}</div>
-                <h2 className='item__content-title'>{title}</h2>
-                <p className='item__content-text'>{excerpt}</p>
-                <button className='item__content-button'></button>
+            <div className={styles.item__content}>
+                <div className={styles.content__category}>
+                    {categories[0].title}
+                </div>
+                <h2 className={styles.content__title}>{title}</h2>
+                <p className={styles.content__text}>{excerpt}</p>
+                <a href='/' className={styles.content__button}>
+                    View Case Study
+                    <img
+                        className={styles.content__svg}
+                        src={arrowIcon}
+                        alt='Button arrow'
+                    />
+                </a>
+                {/* <a href='/' className={styles.content__button}>
+                    View Case Study
+                </a> */}
             </div>
-        </li>
+        </div>
     );
 };
 
