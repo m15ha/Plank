edaa4550aa271338a238c9cc5a384c2a6063a9fa
m15ha <mihail.lazareff@gmail.com>
2022-02-13 10:29:32 -0500

    feat: add burger menu, fix layout

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styles from './styles.module.scss';
+import { ReactComponent as BarsIcon } from '../../assets/bars.svg';
+import { ReactComponent as XmarkIcon } from '../../assets/xmark.svg';
 
 const Filters = ({
     filterHandler,
@@ -8,36 +10,61 @@ const Filters = ({
     isLoading,
     selectedFilter,
 }) => {
-    const { categories } = data; 
+    const { categories } = data;
+    const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+    const handleBurgerClick = () => {
+        setIsBurgerOpen(!isBurgerOpen);
+    };
+
+    const handleFilterClick = filter => {
+        filterHandler(filter);
+        setIsBurgerOpen(false);
+    };
 
     return (
-        <ul className={styles.filter__container}>
-            <li key='all'>
-                <button
-                    className={selectedFilter === 'all' ? styles.active : ''}
-                    onClick={() => filterHandler('all')}
-                >
-                    All
-                </button>
-            </li>
-            {categories &&
-                categories.map(category => {
-                    return (
-                        <li key={category.slug}>
-                            <button
-                                className={
-                                    selectedFilter === category.slug
-                                        ? styles.active
-                                        : ''
-                                }
-                                onClick={() => filterHandler(category.slug)}
-                            >
-                                {category.title}
-                            </button>
-                        </li>
-                    );
-                })}
-        </ul>
+        <>
+            <div className={styles.filter__burger_container}>
+                <BarsIcon onClick={handleBurgerClick} />
+            </div>
+            <ul
+                className={`${styles.filter__container} ${
+                    isBurgerOpen ? styles.filter__container_open : ''
+                }`}
+            >
+                <XmarkIcon onClick={() => setIsBurgerOpen(false)} />
+
+                <li key='all'>
+                    <button
+                        className={
+                            selectedFilter === 'all' ? styles.active : ''
+                        }
+                        onClick={() => handleFilterClick('all')}
+                    >
+                        All
+                    </button>
+                </li>
+                {categories &&
+                    categories.map(category => {
+                        return (
+                            <li key={category.slug}>
+                                <button
+                                    className={
+                                        selectedFilter === category.slug
+                                            ? styles.active
+                                            : ''
+                                    }
+                                    onClick={() =>
+                                        handleFilterClick(category.slug)
+                                    }
+                                >
+                                    {category.title}
+                                </button>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </>
     );
 };
 
